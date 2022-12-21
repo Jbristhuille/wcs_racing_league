62e5d85c60a6431674f0010699c08e52856594a6
Jbristhuille <jbristhuille@gmail.com>
2022-12-21 14:53:49 +0100

    :tada: Setup server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+/**
+ * @ Author: Jbristhuille
+ * @ Create Time: 2022-12-21 13:08:28
+ * @ Description: Main server file
+ */
+
+/* SUMMARY
+    * Imports
+    * Config
+*/
+
+/* Imports */
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const router = require('./router');
+/***/
+
+/* Config */
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+/***/
+
+/* Start server */
+app.listen(process.env.PORT, () => {
+    console.log(`Server start on port ${process.env.PORT}`);
+});
+/***/
---
+++ server/router.js
@@ -0,0 +1,16 @@
+/**
+ * @ Author: Jbristhuille
+ * @ Create Time: 2022-12-21 14:50:03
+ * @ Description: Router file
+ */
+
+/* SUMMARY
+    * Imports
+*/
+
+/* Imports */
+const express = require('express');
+const router = express.Router();
+/***/
+
+module.exports = router;
