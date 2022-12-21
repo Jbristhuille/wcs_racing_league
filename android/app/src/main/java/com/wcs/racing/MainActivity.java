e8992cc8802c5483be24f7be1c8fed214d60b94a
Jbristhuille <jbristhuille@gmail.com>
2022-12-21 13:07:19 +0100

    :tada: Setup project (capcitor and react client)

---
+++ android/app/src/main/java/com/wcs/racing/MainActivity.java
@@ -0,0 +1,5 @@
+package com.wcs.racing;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.sass';
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
